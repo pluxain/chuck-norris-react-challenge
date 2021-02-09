978a1524720ee3d087dd1de99f31f992c16d9995
Pascal Luxain <pascalln.dev@gmail.com>
2021-02-09 21:59:09 +0100

    fetch random joke

--- src/features/jokes/Jokes.tsx
+++ src/features/jokes/Jokes.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RooState } from 'rootReducer';
+import { fetchRandom } from './slice';
 
 export default function Joke() {
+  const { joke } = useSelector((state: RooState) => state.jokes);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (typeof joke === 'undefined') {
+      dispatch(fetchRandom());
+    }
+  }, [joke, dispatch]);
   return (
     <>
-      <h2 className="text-center text-2xl">Random Joke</h2>
+      <h2>{joke?.joke}</h2>
     </>
   );
 }
