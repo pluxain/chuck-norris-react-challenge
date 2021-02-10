757f467e77e2d4f4e146b43e9c02e92f774a1fcc
Pascal Luxain <pascalln.dev@gmail.com>
2021-02-10 11:16:54 +0100

    Add primary and secondary theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: colors.blue,
+      secondary: colors.green,
+      black: colors.black,
+      white: colors.white,
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+    },
   },
   plugins: [],
 };
